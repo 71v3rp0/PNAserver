728e88449caf4998231d818286644039e87d5fe4
71v3rp0 <alexdorofeev2001@gmail.com>
2022-10-27 00:01:13 +0300

    1

--- server/Gumming.h
+++ server/Gumming.h
@@ -1,4 +1,5 @@
 #pragma once
 void decryption(char data[], int gamma[], int messageSize);
 void encryption(char data[], int gamma[], int messageSize);
+void createGamma(int gamma[], int messageSize);
 int getIndex(char symbol);
